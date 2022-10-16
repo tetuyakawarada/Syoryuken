9e2410824e597c0a35d310c8255b6541b6d10e71
tetuyakawarada <tetuya.kawarada@gmail.com>
2022-10-17 08:42:40 +0900

    Initial Commit

---
+++ Syoryuken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Syoryuken
+//
+//  Created by tetsuya on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
